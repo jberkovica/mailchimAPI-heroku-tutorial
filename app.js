1e89d41037f621523af1418be6ac25e94059cd65
jberkovica <jberkovica@evolutiongaming.com>
2020-12-12 18:32:27 +0200

    update to deploy to heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 
 // express app setup
 const app = express();
@@ -31,8 +32,41 @@ app.post("/", function (req, res) {
   };
 
   const jsonData = JSON.stringify(data);
+
+  const APIkey = "ce0da5d33a7eadcacc465f052247cd14-us7";
+  const audienceID = "23bd71a188";
+  const url = "https://us7.api.mailchimp.com/3.0/lists/" + audienceID;
+  const auth = "jberkovica:" + APIkey;
+  const options = {
+    method: "POST",
+    auth: auth,
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
-app.listen(3000, function () {
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+// Heroku will automaticaly chose port for app, so we are not using hardcoded 3000
+// dynamic port
+// but to use it also locally we can use or "||" 
+// to listen to one or to another
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
